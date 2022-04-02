a8fca166e4f2c57dd191ea3f76036f4cd4883812
Rahims609 <rahimboss609@gmail.com>
2022-04-02 21:41:13 +0600

    components added

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h3>About this web page</h3>
+            <p>birani na khawale about bolbo na vai tumi ki koro ami jani na </p>
+        </div>
+    );
+};
+
+export default About;
