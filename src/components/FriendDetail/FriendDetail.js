0170fa452ba0680cebae26f4598d09c81bc312f7
Rahims609 <rahimboss609@gmail.com>
2022-04-03 13:37:09 +0600

    detail added

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
-    
     const {friendId}= useParams();
+    const [friend, setFriend] = useState({})
 
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setFriend(data))
+    },[])
     return (
         <div>
             <h2>This is Detail a Friend : {friendId}</h2>
+            <h3>Name: {friend.name}</h3>
+            <h4>Email: {friend.email}</h4>
+            <p><small>City: {friend.address?.city}</small></p>
+            <p><small>lat: {friend.address?.geo?.lat}, lng:{friend.address?.geo?.lng}</small></p>
              
         </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CustomLink from '../CustomLink/CustomeLink';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h1>Welcomr to my fancy Routing Website!!</h1>
-            <nav>
+            <nav className="nav">
                <p><CustomLink to="/">Home</CustomLink></p>
                <p><CustomLink to="/friends">Friends</CustomLink></p>
                <p><CustomLink to="/about">About</CustomLink></p>
